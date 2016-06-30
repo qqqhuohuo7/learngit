7bad1b7109b6bb7045cd67f08cf0ee90af925bf0
peizhou <peizhou@zjft.com>
2016-06-30 15:28:48 +0800

    debug 2

--- python/als/app/models.py
+++ python/als/app/models.py
@@ -6,8 +6,12 @@ conn = MySQLdb.connect(user="python",passwd="password",host="127.0.0.1")
 conn.select_db("python") #选择database
 curr = conn.cursor() #创建游标
 
-username = 'test01'
-sqlStr = 'insert into `user` (`id`,`username`) values (%d,"%s")' (1,username) #绑定变量防sql注入
+#username = 'test01'
+#sqlStr = 'insert into `user` (`id`,`username`) values (%d,"%s")' (1,username) #绑定变量防sql注入
+
+sqlStr = 'select username from user'
+arr = curr.execute(sqlStr)
+info = curr.fetchmany(arr)
 curr.execute(sqlStr)
 
 class Record():
